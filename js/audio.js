cc8fbe4811415b81510534aa8c469fbfe0a46ac4
Arian Varx <ariantechzone@gmail.com>
2026-01-26 19:23:40 +0530

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,20 +1,8 @@
-export const lofi = document.getElementById("lofi");
-export const rain = document.getElementById("rain");
-export const snow = document.getElementById("snow");
-export const wind = document.getElementById("wind");
-export const fireplace = document.getElementById("fireplace");
-
-export function playBell(type) {
-  document.getElementById(
-    type === "focus" ? "bellFocus" :
-    type === "long" ? "bellLong" : "bellBreak"
-  ).play();
+export function play(audio, volume = 0.6) {
+  audio.volume = volume;
+  audio.play().catch(() => {});
 }
 
-document.getElementById("upload").onchange = e => {
-  const file = e.target.files[0];
-  if (file) {
-    lofi.src = URL.createObjectURL(file);
-    lofi.play();
-  }
-};
+export function stop(audio) {
+  audio.pause();
+}
