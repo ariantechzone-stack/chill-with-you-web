1efd25c48601a971dc96dfbe8c327d4967437a3f
Arian Varx <ariantechzone@gmail.com>
2026-01-26 13:56:48 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
+const audio = document.getElementById("lofi");
+const btn = document.getElementById("toggleFocus");
+
+let playing = false;
+
+btn.addEventListener("click", () => {
+  playing = !playing;
+
+  if (playing) {
+    audio.play();
+    btn.textContent = "Stop Focus";
+  } else {
+    audio.pause();
+    btn.textContent = "Focus Mode";
+  }
+});
 
