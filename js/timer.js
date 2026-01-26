bc83a0905676c0b2aa9dd6ba9730531c1040910e
Arian Varx <ariantechzone@gmail.com>
2026-01-26 19:17:14 +0530

    Update timer.js

--- js/timer.js
+++ js/timer.js
@@ -1,56 +1,30 @@
 import { playBell } from "./audio.js";
-import { setScene } from "./scene.js";
+import { setCompanion } from "./companion.js";
 
-let focus = 25 * 60;
-let brk = 5 * 60;
-let long = 15 * 60;
-
-let cycles = 0;
-let time = focus;
-let running = false;
-let isFocus = true;
-let interval;
+let focus = 1500, breakT = 300;
+let time = focus, running = false, focusMode = true;
 
 const timer = document.getElementById("timer");
-const label = document.getElementById("timerLabel");
 
 function render() {
   timer.textContent =
-    String(Math.floor(time / 60)).padStart(2, "0") + ":" +
-    String(time % 60).padStart(2, "0");
-}
-
-function switchMode() {
-  isFocus = !isFocus;
-
-  if (isFocus) {
-    time = focus;
-    label.textContent = "Focus Time";
-    playBell("focus");
-    setScene("day");
-  } else {
-    cycles++;
-    const longBreak = cycles % 4 === 0;
-    time = longBreak ? long : brk;
-    label.textContent = longBreak ? "Long Break" : "Break";
-    playBell(longBreak ? "long" : "break");
-    setScene("night");
-  }
+    `${String(Math.floor(time/60)).padStart(2,"0")}:${String(time%60).padStart(2,"0")}`;
 }
 
 document.getElementById("startPause").onclick = () => {
   if (!running) {
     running = true;
-    interval = setInterval(() => {
-      time--;
-      render();
-      if (time <= 0) switchMode();
+    setInterval(() => {
+      time--; render();
+      if (time <= 0) {
+        focusMode = !focusMode;
+        time = focusMode ? focus : breakT;
+        playBell(focusMode ? "focus" : "break");
+        document.getElementById("scene").classList.toggle("night");
+        setCompanion(focusMode ? "focus" : "break");
+      }
     }, 1000);
-  } else {
-    clearInterval(interval);
-    running = false;
   }
 };
 
 render();
-
