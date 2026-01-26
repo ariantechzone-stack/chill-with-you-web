824281bd6e9f0a53384947b8e87e3c029872a5af
Arian Varx <ariantechzone@gmail.com>
2026-01-26 19:31:50 +0530

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -6,3 +6,7 @@ export function play(audio, volume = 0.6) {
 export function stop(audio) {
   audio.pause();
 }
+export function toggleFireplace(audio, on) {
+  if (on) audio.play().catch(()=>{});
+  else audio.pause();
+}
