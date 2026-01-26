28ac9cc460348f8b0561c23a27c77d9b71a7b1ad
Arian Varx <ariantechzone@gmail.com>
2026-01-26 19:08:55 +0530

    Update scene.js

--- js/scene.js
+++ js/scene.js
@@ -1,1 +1,13 @@
+const scene = document.getElementById("scene");
+const rain = document.getElementById("rain");
+
+export function setScene(mode) {
+  if (mode === "night") {
+    scene.classList.add("night");
+    rain.volume = 0.4;
+    rain.play();
+  } else {
+    scene.classList.remove("night");
+  }
+}
 
